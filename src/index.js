90de95c8e7ca40d20ba205f567d1891598becf98
jeff-h <jeffvh@outlook.com>
2020-05-03 20:56:10 +1000

    setup a web socket server and connect to it from client

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Jeff H'
-    })
+io.on('connection', () => {
+    console.log('New WebSocket connection')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
